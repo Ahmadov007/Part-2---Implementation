1a39cba7d3b00dcb39cd947032f1df54f7d82232
Meti <mohaf002@mymail.unisa.edu.au>
2022-10-16 21:00:45 +1030

    first commit

---
+++ atd.py
@@ -0,0 +1,21 @@
+class AllThatDice:
+    def __init__(self):
+        self.__registeredPlayers = []
+
+    def run(self):
+        pass
+
+    def registerPlayer(self, name):
+        pass
+
+    def displayMenu(self):
+        pass
+
+    def findPlayer(self, name):
+        pass
+
+    def displayGameOption(self):
+        pass
+
+    
+        
---
+++ player.py
@@ -0,0 +1,37 @@
+class Player:
+    def __init__(self, name, chipBalance, numOfGamesPlayed, numOfGamesWon):
+        self.__name = name
+        self.__chipBalance = chipBalance
+        self.__numOfGamesPlayed = numOfGamesPlayed
+        self.__numOfGamesWon = numOfGamesWon
+
+    def getName(self):
+        """Returns the player name"""
+        return self.__name
+
+    def setName(self, name):
+        """Sets the player name"""
+        self.__name = name
+
+    def getChipBalance(self):
+        """Returns the players chip balance"""
+        return self.__chipBalance
+
+    def setChipBalnce(self, chipBalance):
+        """Sets the players chip balance"""
+        self.__chipBalance = chipBalance
+
+    def incrementGamesPlayed(self, num):
+        """Increment the number of games played by num digit"""
+        self.__numOfGamesPlayed += num
+
+    def setNumberOfGamesWon(self, num):
+        """Sets the number of games won by num value"""
+        self.__numOfGamesWon = num
+
+    def getNumberOfGamesWon(self):
+        """Returns the number of games won"""
+        return self.__numOfGamesWon
+
+    
+
