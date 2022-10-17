07279ed96444d18854f01c9f65dcc3d0f7defdff
Meti <mohaf002@mymail.unisa.edu.au>
2022-10-17 17:16:39 +1030

    commit 4

--- atd.py
+++ atd.py
@@ -3,23 +3,23 @@ class AllThatDice:
         self.__registeredPlayers = []
 
     def run(self):
+        '''Runs AllThatDice'''
         pass
 
     def registerPlayer(self, name):
+        '''Registers a player using the name parameter'''
         pass
 
     def displayMenu(self):
+        '''Displays the menu'''
         pass
 
     def findPlayer(self, name):
+        '''Finds a player using the name parameter'''
         pass
 
     def displayGameOption(self):
+        '''Displays the game option'''
         pass
 
-    def testFunction(self):
-        pass
-    
-    def testFunction2(self):
-        print("Hello world")
         
