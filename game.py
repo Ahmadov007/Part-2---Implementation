2c85dac685c4a0691e6a274e97b3391a49c1cf9f
Meti <mohaf002@mymail.unisa.edu.au>
2022-10-17 17:30:25 +1030

    commit 5

---
+++ game.py
@@ -0,0 +1,20 @@
+class Game:
+    def __init__(self, name, maxPlayer, minPlayer):
+        self.__name = name
+        self.__maximumPLayer = maxPlayer
+        self.__minimumPLayer = minPlayer
+        self.__playerList = []
+
+    def findPLayer(self, name):
+        pass
+    
+    def chipBid(self):
+        pass
+
+    def shifterValue(self, val):
+        pass
+
+    def play(self):
+        pass
+
+    
